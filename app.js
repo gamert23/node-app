09bf644a17eb472e786e293c2cde12a5efb68aec
Ratchanon <game@Macintosh.local>
2021-12-09 22:33:24 +0700

    Test express

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const express = require('express')
 const app = express()
 
 // Middleware
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
+  console.log("always run!")
+  next()
+})
+
+app.use('/add-product', (req, res, next) => {
+  console.log("add product page middleware")
+  res.send('<form action="/products" method="POST"> <input type="text" /> <button type="submit"> Add Product </button> </form>')
+})
+
+app.use('/products', (req, res, next) => {
+  console.log(req.body)
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
   console.log("another middleware")
   res.send('<h1> Hello From Express! </h1>')
 })
