794a8b907fe97ca5955538c0545cf88449758697
Ratchanon <ratchanon.p@cjworx.com>
2021-12-09 08:47:34 +0700

    Update gitignore and install expressjs

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const http = require('http');
 const routes = require('./routes');
+const express = require('express')
 
+const app = express()
 
-const server = http.createServer(routes.handler);
+// Middleware
+app.use((req, res, next) => {
+  console.log("another middleware")
+  res.send('<h1> Hello From Express! </h1>')
+})
 
-server.listen(3002)
+app.listen(3002)
+
+// const server = http.createServer(routes.handler);
+// const server = http.createServer(app);
+
+// server.listen(3002)
